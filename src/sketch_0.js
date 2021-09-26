fc03407a1f861834511f53af5a041ba1048480d5
inaridarkfox4231 <sea_gull6389-4m@ka.tnc.ne.jp>
2021-09-27 01:06:41 +0900

    ちょっと調べただけ

--- src/sketch_0.js
+++ src/sketch_0.js
@@ -1,26 +1,15 @@
+function preload(){
+  console.log("hello!");
+}
+let func = () => {return 2;}
 function setup() {
-  createCanvas(400, 400, WEBGL);
-  strokeWeight(6);
+  createCanvas(400, 400);
+  fill(0, 128, 255);
+  rect(20, 20, 80, 80);
 }
 
 function draw() {
   background(0);
-  beginShape();
-  stroke(255,0,0);
-  fill(0,0,255);
-  vertex(0,0,0);
-  stroke(255);
-  fill(255);
-  vertex(100,0,0);
-  stroke(255,0,0);
-  fill(0,0,255);
-  vertex(100,100,0);
-  stroke(255);
-  fill(255);
-  vertex(0,100,0);
-  stroke(255,0,0);
-  fill(0,0,255);
-  vertex(0,0,0);
-  endShape();
-  noLoop();
+  circle(200, 200, 40);
+  if(frameCount==3){noLoop();}
 }
